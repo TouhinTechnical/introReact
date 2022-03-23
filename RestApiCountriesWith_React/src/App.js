cc833eb4b4a08a309e56cf301acf66a8921d839c
TouhinTechnical <touhidulislam1090@gmail.com>
2022-03-23 19:51:02 +0600

    ...

--- RestApiCountriesWith_React/src/App.js
+++ RestApiCountriesWith_React/src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 import LoadCountries from './components/Country/Country';
+import Footer from './components/Footer/footer';
 import MenuBar from './components/Header/header';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <MenuBar></MenuBar>
       <LoadCountries></LoadCountries>
+      <Footer></Footer>
     </div>
   );
 }
